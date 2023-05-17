678003f8471e6cd71387abefbfa4f0c92598b214
Amal C P <acp070146@gmail.com>
2023-05-17 23:44:02 +0530

    headder and error page

--- src/api/index.tsx
+++
@@ -1,8 +0,0 @@
-
-const tokenurl = `token/`;
-const refreshtokenurl = `token/refresh/`;
-const registerurl = `register/`;
-const todourl = `todo/`;
-const todoupdateurl = `todo/<str:pk>/`;
-
-export {tokenurl,refreshtokenurl,registerurl,todoupdateurl,todourl}
